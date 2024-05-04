6130a507c77f1eb848728f5a427118fc2658e0f3
Florian Ohrel <ohrel.florian@pm.me>
2024-05-04 18:50:16 +0200

    fix: burgermenu animation, add favicon, add manifest files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import shield from "@kindspells/astro-shield";
+import favicons from "astro-favicons";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-import shield from "@kindspells/astro-shield";
-import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 
+// https://astro.build/config
 export default defineConfig({
+  site: "https://flohrel.com",
   prefetch: true,
   i18n: {
     defaultLocale: "en",
@@ -13,8 +18,31 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  integrations: [
-    tailwind({ nesting: true }),
-    icon({ include: { "fa6-brands": ["github", "linkedin"], "material-symbols": ["left-panel-open-sharp", "left-panel-close-sharp", "open-in-new", "mail-outline-rounded"], "mingcute": ["github-line"] } }), shield({})],
-
+  integrations: [tailwind({
+    nesting: true
+  }), icon({
+    include: {
+      "fa6-brands": ["github", "linkedin"],
+      "material-symbols": ["left-panel-open-sharp", "left-panel-close-sharp", "open-in-new", "mail-outline-rounded"],
+      "mingcute": ["github-line"]
+    }
+  }), shield({
+    securityHeaders: {
+      "Content-Security-Policy": "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none'; worker-src 'none'; base-uri 'none'; frame-ancestors 'none'; manifest-src 'self'; block-all-mixed-content; upgrade-insecure-requests; disown-opener; reflected-xss block; referrer no-referrer; report-to csp-endpoint"
+    }
+  }), sitemap(), robotsTxt(), favicons({
+    masterPicture: "./src/favicon.svg",
+    emitAssets: true,
+    appName: "Portfolio",
+    appDescription: "Florian Ohrel's portfolio",
+    dir: "auto",
+    lang: "en-US",
+    display: "standalone",
+    orientation: "any",
+    start_url: "/",
+    background: "#fff",
+    theme_color: "#fff",
+    faviconsDarkMode: true,
+    appleStatusBarStyle: "black-translucent",
+  })]
 });
